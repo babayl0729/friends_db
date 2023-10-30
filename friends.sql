e259bd1f72c6b0367251074ae818e532cb4f0820
Lawrence Babay <lbabay0729@live.com>
2023-10-29 21:33:03 -0400

    first commit

---
+++ friends.sql
@@ -0,0 +1,12 @@
+CREATE TABLE friends(
+    id INTEGER,
+    name TEXT,
+    birthday DATE
+);
+
+INSERT INTO friends(id, name, birthday)
+VALUES(0, 'Ororo Munroe', 05/30/1940),
+    (1, 'Lauren Isabella babay', 'July 28, 2013'),
+    (2, 'Lyzander Trumata', 'November 1, 2013'),
+    (3, 'Jaiden Smith', 'March 17, 2014'),
+    (4, 'William Henry Wilson', 'August 22, 2021');
