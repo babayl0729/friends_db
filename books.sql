d49268a1b74f038997daa7ceb4fd4e1e42eb97f6
babayl0729 <babayl0729@gmail.com>
2023-12-11 21:16:36 -0700

    added new values

--- books.sql
+++ books.sql
@@ -57,5 +57,10 @@ CONCAT(author_lname, ',', author_fname) AS 'author',
 CONCAT(stock_quantity, ' ', 'in stock') AS quantiy
 FROM books;
 
+INSERT INTO books(title, author_fname, author_lname, released_year, stock_quantity, pages)
+VALUES('10% Happier', 'Dan', 'Harris', 2014, 29, 256),
+    ('fake_book', 'Freida', 'Harris', 2001, 287, 428),
+    ('Lincoln In The Bardo', 'George', 'Saunders', 2017, 1000, 367);
+
 
 
