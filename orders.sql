2ca12aa2f083b59cce2203cd60bf913a022ee5f6
babayl0729 <babayl0729@gmail.com>
2024-01-27 15:34:34 -0700

    left join orders to customers

--- orders.sql
+++ orders.sql
@@ -11,4 +11,12 @@ VALUES('2016-02-10',99.99,1),
 ('2017-11-11', 35.50, 1), 
 ('2014-12-12', 800.67, 2), 
 ('2015-01-03', 12.50, 2), 
-('1999-04-11', 450.25, 5); 
+('1999-04-11', 450.25, 5);
+
+--left join orders to customers--
+SELECT  
+    order_date, amount, first_name, last_name 
+FROM 
+    orders 
+LEFT JOIN 
+    customers ON orders.customer_id = customers.id; 
