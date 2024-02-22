74cbbfd21c513df2d68a0fb2ac4f48ddbe07adb0
babayl0729 <babayl0729@gmail.com>
2024-02-21 19:10:47 -0700

    many to many

---
+++ reviewers.sql
@@ -0,0 +1,15 @@
+CREATE TABLE reviewers(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO reviewers (first_name, last_name) VALUES
+    ('Thomas', 'Stoneman'),
+    ('Wyatt', 'Skaggs'),
+    ('Kimbra', 'Masters'),
+    ('Domingo', 'Cortes'),
+    ('Colt', 'Steele'),
+    ('Pinkie', 'Petit'),
+    ('Marlon', 'Crafford');
+
