29918ce975f41138470a98756ea49c48c685c33e
babayl0729 <babayl0729@gmail.com>
2024-03-12 19:13:09 -0600

    shirts table

--- shirts_db.sql
+++ shirts_db.sql
@@ -34,10 +34,8 @@ WHERE article = 'polo shirt';
 UPDATE shirts SET last_worn = 0
 WHERE last_worn = 15;
 
-UPDATE shirts SET shirt_size = 'XS'
-WHERE color = 'white';
-
-UPDATE shirts SET color = 'off white'
+UPDATE shirts SET color = 'off white',
+shirt_size = 'XS'
 WHERE color = 'white';
 
 DELETE FROM shirts 
